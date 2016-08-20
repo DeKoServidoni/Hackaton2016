5f16b21ad1275a3ee623de337bad94d7a4e81e69
Clêrton Leal <clertonleal@gmail.com>
2016-08-20 14:00:42 -0300

    adding actions to search

--- Ágora/SearchProductsViewController.swift
+++ Ágora/SearchProductsViewController.swift
@@ -13,13 +13,16 @@ class SearchProductViewController: UIViewController, UITableViewDelegate, UITabl
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet var myTableView: UITableView!
     
+    var initialList = ProductStore.getAllProducts()
     var list = ProductStore.getAllProducts()
-    var selectedList: [Product] = []
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         myTableView.allowsMultipleSelection = true
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -28,39 +31,55 @@ class SearchProductViewController: UIViewController, UITableViewDelegate, UITabl
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("myCell")! as UITableViewCell
-        cell.selectionStyle = .None
         let product = list[indexPath.row]
+        
+        if (product.checked) {
+            cell.accessoryType = UITableViewCellAccessoryType.Checkmark
+        } else {
+            cell.accessoryType = UITableViewCellAccessoryType.None
+        }
+        
+        cell.selectionStyle = .None
         cell.textLabel?.text = product.name
         return cell
     }
     
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.cellForRowAtIndexPath(indexPath)?.accessoryType = UITableViewCellAccessoryType.Checkmark
-        selectedList.append(list[indexPath.row])
-        print(selectedList)
-        searchBar.resignFirstResponder()
-    }
-    
-    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        searchBar.resignFirstResponder()
+        var product = list[indexPath.row]
+        product.checked = true
         
+        initialList = initialList.map { (Product) -> Product in
+            if (product.name == Product.name) {
+                return product;
+            }
+            return Product
+        }
+        
+        searchBar.resignFirstResponder()
     }
     
     func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.cellForRowAtIndexPath(indexPath)?.accessoryType = UITableViewCellAccessoryType.None
-        selectedList = selectedList.filter({ (Product) -> Bool in
-            return Product.name != list[indexPath.row].name
-        })
-        print(selectedList)
+        var product = list[indexPath.row]
+        product.checked = false
+        
+        initialList = initialList.map { (Product) -> Product in
+            if (product.name == Product.name) {
+                return product;
+            }
+            return Product
+        }
+        
         searchBar.resignFirstResponder()
     }
     
     func searchBar(searchBar: UISearchBar, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
         let searchText = self.searchBar.text! + text
         if (searchText.isEmpty) {
-            list = ProductStore.getAllProducts()
+            list = initialList
         } else {
-            list = ProductStore.getAllProducts().filter { (Product) -> Bool in
+            list = initialList.filter { (Product) -> Bool in
                 return Product.name.containsString(searchText)
             }
         }
