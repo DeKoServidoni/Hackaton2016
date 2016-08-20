f4d83d2831b5912ca6a93e71ca12510895ba8d4f
Andre Servidoni <avenuecode@Avenues-MacBook-Pro-3.local>
2016-08-20 10:41:31 -0300

    Add the initial structure
    TabBar view controller

---
+++ Ágora/MyListsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MyListsViewController.swift
+//  Ágora
+//
+//  Created by Avenue Code on 8/20/16.
+//  Copyright © 2016 Avenue Code. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyListsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        
+    }
+}
