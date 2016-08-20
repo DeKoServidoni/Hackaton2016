fd97f8cae08eaaef099af377801f98126e16074a
Andre Servidoni <avenuecode@Avenues-MacBook-Pro-3.local>
2016-08-20 08:41:14 -0300

    Initial commit

---
+++ Ágora/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ágora
+//
+//  Created by Avenue Code on 8/20/16.
+//  Copyright © 2016 Avenue Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
